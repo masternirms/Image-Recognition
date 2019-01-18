38193cc286698929fd8a86b99383a2e01fa34838
masternirms <nirms.rajesh@gmail.com>
2019-01-18 14:32:32 +0000

    Finished debugging and making it more user-friendly

--- KNNClassifier.py
+++ KNNClassifier.py
@@ -1,25 +1,3 @@
-"""
-This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
-When should I use this example?
-This example is useful when you wish to recognize a large set of known people,
-and make a prediction for an unknown person in a feasible computation time.
-Algorithm Description:
-The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
-in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
-in its training set, and performing a majority vote (possibly weighted) on their label.
-For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
-and two images of Obama, The result would be 'Obama'.
-* This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
-Usage:
-1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
-   with a sub-directory for each known person.
-2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
-   want to save the model to disk so you can re-use the model without having to re-train it.
-3. Call 'predict' and pass in your trained model to recognize the people in an unknown image.
-NOTE: This example requires scikit-learn to be installed! You can install it with pip:
-$ pip3 install scikit-learn
-"""
-
 import math
 from sklearn import neighbors
 import os
@@ -28,17 +6,18 @@ import pickle
 from PIL import Image, ImageDraw
 import face_recognition
 from face_recognition.face_recognition_cli import image_files_in_folder
+from fnmatch import fnmatch
+from shutil import move, copy2
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG', 'PNG'}
 
-
-def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
+def train(train_directory_path, model_save_path=None, n_neighbors=None, knn_algo='ball_tree'):
     """
     Trains a k-nearest neighbors classifier for face recognition.
-    :param train_dir: directory that contains a sub-directory for each known person, with its name.
-     (View in source code to see train_dir example tree structure)
+    :param train_directory_path: directory that contains a sub-directory for each known person, with its name.
+     (View in source code to see train_directory_path example tree structure)
      Structure:
-        <train_dir>/
+        <train_directory_path>/
         ├── <person1>/
         │   ├── <somename1>.jpeg
         │   ├── <somename2>.jpeg
@@ -50,36 +29,33 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
     :param model_save_path: (optional) path to save model on disk
     :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
     :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
-    :param verbose: verbosity of training
     :return: returns knn classifier that was trained on the given data.
     """
     X = []
     y = []
 
     # Loop through each person in the training set
-    for class_dir in os.listdir(train_dir):
-        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+    for class_directory_path in os.listdir(train_directory_path):
+        if not os.path.isdir(os.path.join(train_directory_path, class_directory_path)):
             continue
 
         # Loop through each training image for the current person
-        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+        for img_path in image_files_in_folder(os.path.join(train_directory_path, class_directory_path)):
             image = face_recognition.load_image_file(img_path)
             face_bounding_boxes = face_recognition.face_locations(image)
 
-            if len(face_bounding_boxes) != 1:
-                # If there are no people (or too many people) in a training image, skip the image.
-                if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
-            else:
-                # Add face encoding for current image to the training set
+            if len(face_bounding_boxes) == 1:
+                # Add face encoding for current image to the training set as long as there is only one face in the picture
                 X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                y.append(class_dir)
+                y.append(class_directory_path)
 
     # Determine how many neighbors to use for weighting in the KNN classifier
     if n_neighbors is None:
         n_neighbors = int(round(math.sqrt(len(X))))
-        if verbose:
-            print("Chose n_neighbors automatically:", n_neighbors)
+
+    if len(X) == 0 or len(y) == 0:
+        print("Could not detect any faces.")
+        quit()
 
     # Create and train the KNN classifier
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
@@ -93,10 +69,10 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
     return knn_clf
 
 
-def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
+def predict(predict_directory_path, knn_clf=None, model_path=None, distance_threshold=0.6):
     """
     Recognizes faces in given image using a trained KNN classifier
-    :param X_img_path: path to image to be recognized
+    :param predict_directory_path: path to image to be recognized
     :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
     :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
     :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
@@ -104,8 +80,8 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
     :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
         For faces of unrecognized persons, the name 'unknown' will be returned.
     """
-    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-        raise Exception("Invalid image path: {}".format(X_img_path))
+    if not os.path.isfile(predict_directory_path) or os.path.splitext(predict_directory_path)[1][1:] not in ALLOWED_EXTENSIONS:
+        raise Exception("Invalid image path: {}".format(predict_directory_path))
 
     if knn_clf is None and model_path is None:
         raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
@@ -116,7 +92,7 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
             knn_clf = pickle.load(f)
 
     # Load image file and find face locations
-    X_img = face_recognition.load_image_file(X_img_path)
+    X_img = face_recognition.load_image_file(predict_directory_path)
     X_face_locations = face_recognition.face_locations(X_img)
 
     # If no faces are found in the image, return an empty result.
@@ -133,57 +109,101 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
     # Predict classes and remove classifications that aren't within the threshold
     return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
+def copy_files(name, source_file_path, copy_directory_path):
+    copy2_directory_path = os.path.join(copy_directory_path, name)
+    if os.path.exists(copy2_directory_path):
+        copy2(source_file_path, copy2_directory_path)
+    else:
+        os.mkdir(copy2_directory_path)
+        copy2(source_file_path, copy2_directory_path)
 
-def show_prediction_labels_on_image(img_path, predictions):
-    """
-    Shows the face recognition results visually.
-    :param img_path: path to image to be recognized
-    :param predictions: results of the predict function
-    :return:
-    """
-    pil_image = Image.open(img_path).convert("RGB")
-    draw = ImageDraw.Draw(pil_image)
 
-    for name, (top, right, bottom, left) in predictions:
-        # Draw a box around the face using the Pillow module
-        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+def open_photos(nameInput, retrieve_directory_path):
+    retrieve2_directory_path = os.path.join(retrieve_directory_path, nameInput)
+    if os.path.exists(retrieve2_directory_path):
+        for path, subdirs, files in os.walk(retrieve2_directory_path):
+            for name in files:
+                photo = Image.open(os.path.join(path, name))
+                photo.show()
 
-        # There's a bug in Pillow where it blows up with non-UTF-8 text
-        # when using the default bitmap font
-        name = name.encode("UTF-8")
-
-        # Draw a label with a name below the face
-        text_width, text_height = draw.textsize(name)
-        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-
-    # Remove the drawing library from memory as per the Pillow docs
-    del draw
-
-    # Display the resulting image
-    pil_image.show()
+    else:
+        print("Invalid input.")
+        quit()
 
 
 if __name__ == "__main__":
+
+    fileList = []
+    pattern = "*.png"
+    pattern2 = "*.PNG"
+    pattern3 = "*.JPG"
+    pattern4 = "*.jpg"
+    pattern5 = "*.jpeg"
+
     # STEP 1: Train the KNN classifier and save it to disk
     # Once the model is trained and saved, you can skip this step next time.
-    print("Training KNN classifier...")
-    classifier = train("knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
-    print("Training complete!")
+    trainBoolean = input("Do you want to train a model? (Y/N)     ")
+    if trainBoolean == "Y":
+        train_directory_path = input("Enter the directory path that contains the photos you want to train your model on:     ")
+        if os.path.isdir(train_directory_path):
+            print("Training KNN classifier...")
+            classifier = train(train_directory_path, model_save_path="trained_knn_model.clf")
+            print("Training complete!")
+        else:
+            print("Directory does not exist.")
+            quit()
+    elif trainBoolean != "N":
+        print("Invalid input.")
+        quit()
+
 
     # STEP 2: Using the trained classifier, make predictions for unknown images
-    for image_file in os.listdir("knn_examples/test"):
-        full_file_path = os.path.join("knn_examples/test", image_file)
+    predictBoolean = input("Do you want to predict the faces in your photos? (Y/N)     ")
+    if predictBoolean == "Y":
+        predict_directory_path = input("Enter the directory path that contains the photos you want to use your model on:     ")
+        copy_directory_path = input("Enter the directory path that you want to copy the photos to:     ")
+        if os.path.isdir(predict_directory_path) and os.path.isdir(copy_directory_path):
+            for path, subdirs, files in os.walk(predict_directory_path):
+                for name in files:
+                    fileList.append(os.path.join(path, name))
 
-        print("Looking for faces in {}".format(image_file))
+            for file_path in fileList:
+                if fnmatch(file_path, pattern) or fnmatch(file_path, pattern2) or fnmatch(file_path, pattern3) or fnmatch(file_path, pattern4) or fnmatch(file_path, pattern5):
 
-        # Find all people in the image using a trained classifier model
-        # Note: You can pass in either a classifier file name or a classifier model instance
-        predictions = predict(full_file_path, model_path="trained_knn_model.clf")
+                    print("Looking for faces in {}".format(file_path))
 
-        # Print results on the console
-        for name, (top, right, bottom, left) in predictions:
-            print("- Found {} at ({}, {})".format(name, left, top))
+                    # Find all people in the image using a trained classifier model
+                    # Note: You can pass in either a classifier file name or a classifier model instance
+                    predictions = predict(file_path, model_path="trained_knn_model.clf")
 
-        # Display results overlaid on an image
-        show_prediction_labels_on_image(os.path.join("knn_examples/test", image_file), predictions)
+                    # Print results on the console
+                    for name, (top, right, bottom, left) in predictions:
+                        print("- Found {} at ({}, {})".format(name, left, top))
+                        if name != "unknown":
+                            copy_files(name, file_path, copy_directory_path)
+        elif not(os.path.isdir(predict_directory_path)) and not(os.path.isdir(copy_directory_path)):
+            print("Both directories do not exist.")
+            quit()
+        elif not(os.path.isdir(copy_directory_path)):
+            print("The directory you want to copy the photos to does not exist.")
+            quit()
+        elif not(os.path.isdir(predict_directory_path)):
+            print("The directory that contains the photos you want to use the model on does not exist.")
+            quit()
+
+    elif predictBoolean != "N":
+        print("Invalid input.")
+        quit()
+
+    #STEP 3: Based on the user input, you can retrieve the photos you want.
+    retrieveBoolean = input("Do you want to retrieve any of the photos you have classified. (Y/N)     ")
+    if retrieveBoolean == "Y":
+        if predictBoolean == "N":
+            retrieve_directory_path = input("Enter the directory path that you want to retrieve the photos from:     ")
+        else:
+            retrieve_directory_path = copy_directory_path
+        nameInput = input("Enter the name of the person whose photos you would like to retrieve:     ")
+        open_photos(nameInput,retrieve_directory_path)
+    elif retrieveBoolean != "N":
+        print("Invalid input.")
+        quit()
